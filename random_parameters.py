7f40a5a576701a7fe00f5c9a66daa54676f36e69
13PARADOX13 <wdg42ru@gmail.com>
2024-12-16 12:46:11 +0300

    random_parameters.py

--- random_parameters.py
+++ random_parameters.py
@@ -4,6 +4,9 @@ def single_root_words(root_word, *other_words):
     for i in other_words_list:
         if root_word.lower() in i.lower():
             same_words.append(i)
+        elif i.lower() in root_word.lower():
+            same_words.append(i)
     return same_words
 
 print(single_root_words('air', 'AiRplane', 'Airborne', 'ActioN', 'agrEE', 'AIRCRAFT'))
+print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
